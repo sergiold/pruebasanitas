cee46b447b1e2d1f480c8eacc72e62d44c5ae53a
Sergio Lenoir <solr@gft.com>
2022-01-25 13:46:30 +0100

    feat: springboot initializr skeleton project

---
+++ src/main/java/com/sanitas/SanitasApplication.java
@@ -0,0 +1,13 @@
+package com.sanitas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SanitasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SanitasApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sanitas/SanitasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sanitas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SanitasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
